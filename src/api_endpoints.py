3dc19121b578fcdb36fc099fe5ecf36b65f56191
st-randers <soren.langbo.sloto.thomassen@randers.dk>
2025-01-24 13:29:57 +0100

    Correctly parse JSON payload from content-type application/x-www-form-urlencoded

--- src/api_endpoints.py
+++ src/api_endpoints.py
@@ -1,5 +1,6 @@
 import logging
 import time
+import json
 
 from datetime import timedelta
 from flask import Blueprint, Response, request, jsonify
@@ -18,9 +19,14 @@ api_endpoints = Blueprint('api-endpoints', __name__, url_prefix='/api')
 @api_endpoints.route('/webhook', methods=['POST'])
 def webhook_projects():
     if request.headers.get('Content-Type') == 'application/x-www-form-urlencoded':
-        payload = request.get_json()
-        logger.info('Webhook received: ' + str(payload))
-        return jsonify(True), 200
+        payload = request.form.to_dict()
+        try:
+            json_payload = json.loads(list(payload.keys())[0])
+            logger.info('Webhook received: ' + str(json_payload))
+            return jsonify(True), 200
+        except (json.JSONDecodeError, IndexError) as e:
+            logger.error('Failed to parse payload: ' + str(e))
+            return Response('Invalid payload', status=400)
 
     else:
         return Response('Content-Type must be application/x-www-form-urlencoded', status=400)
